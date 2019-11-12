c21055ee004f06235fc93b8c4240541fbc55fa34
Arturo0234 <arturocerrato57@gmail.com>
2019-11-11 20:27:04 -0600

    Create Circulo.java

---
+++ tareamodulo5/Circulo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpers;
+
+/**
+ *
+ * @author Usuario1
+ */
+public class Circulo extends Formas{
+    
+    
+      
+  
+
+    
+  public Circulo(){
+      
+      setColor("Rojo");  
+      setRadio(4);
+
+
+  }
+            
+  @Override
+      public String getInformacionFiguras() {
+        return "Información del Circulo";
+
+}}
